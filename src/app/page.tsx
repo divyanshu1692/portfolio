f8c64e00326d82aed55b2e5a60f4492e1f698e7f
Divyanshu Bisht <divyanshu.bisht@cynoteck.com>
2025-01-17 12:12:59 +0530

    resolved build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Head from "next/head";
 import Navbar from "../components/Navigation/Navbar"
 import Hero from "../components/Home/Hero"
@@ -17,22 +15,20 @@ export default function Home() {
     <>
       <Head>
         <title>Portfolio : Divyanshu Bisht</title>
-        {/* <meta name={'title'} content={'Portfolio : Divyanshu Bisht'} />
+        <meta name={'title'} content={'Portfolio : Divyanshu Bisht'} />
         <meta charSet={'UTF-8'} />
         <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
-        <meta name={'description'} content={'Divyanshu Bisht Skilled in Django & PostgreSQL, MERN Stack, Cloud(GCP, AWS), and Container(Docker, ECS) based technology with hands-on experience in design, document, and creation of scalable software. Knowledge of data warehouse, analytical tool, database query languages(MySQL), Web servers (e.g. Nginx) ,and UI/UX design.'} />
+        <meta name={'description'} content={'I am an experienced Full Stack Developer with a passion for creating efficient and user-friendly web applications. With a proven ability to optimize front-end and back-end systems, I strive to deliver exceptional digital experiences. My expertise spans technologies like React.js, Node.js, MongoDB, and Next.js, along with a strong foundation in problem-solving and data-driven solutions.'} />
         <meta name={'author'} content={'Divyanshu Bisht'} />
         <meta name={'copyright'} content={'Divyanshu Bisht'} />
         <meta name={'application-name'} content={'Divyanshu Bisht Portfolio'} />
 
         <meta property={'og:site_name'} content={'Divyanshu Bisht Portfolio'} />
-        <meta property={'og:type'} content={'ogType'} />
-        <meta property={'og:url'} content={'https://Divyanshu.kodeweich.com'} />
         <meta property={'og:title'} content={'Divyanshu Bisht Portfolio'} />
-        <meta property={'og:description'} content={'Passionate Full Stack Engineer with 3 years of experience in multi-cloud infrastructure provisioning and management. Skilled in deploying and monitoring using Terraform, AWS SAM, and Google Cloud Resource Manager.'} />
+        <meta property={'og:description'} content={'I am an experienced Full Stack Developer with a passion for creating efficient and user-friendly web applications. With a proven ability to optimize front-end and back-end systems, I strive to deliver exceptional digital experiences. My expertise spans technologies like React.js, Node.js, MongoDB, and Next.js, along with a strong foundation in problem-solving and data-driven solutions.'} />
         <meta property={'og:image'} content={'/images/favicon.ico'} />
         <meta property={'og:image:alt'} content={'Divyanshu Bisht Portfolio Icon'}></meta>
-        <meta property={'og:locale'} content={'en'} /> */}
+        <meta property={'og:locale'} content={'en'} />
       </Head>
       <header>
         <Navbar />
